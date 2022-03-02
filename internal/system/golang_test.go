54ba656bfb1911dfd619628ebbfa6b6274985f55
Lone Peon <lonepeon@protonmail.com>
2022-03-02 18:11:21 +0100

    Add system module to get current system go version

---
+++ internal/system/golang_test.go
@@ -0,0 +1,38 @@
+package system_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/lonepeon/cicd/internal/system"
+	"github.com/lonepeon/golib/testutils"
+)
+
+func TestIntegration(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	t.Run("TestGoVersionSuccess", testGoVersionSuccess)
+	t.Run("TestGoVersionFail", testGoVersionFail)
+}
+
+func testGoVersionSuccess(t *testing.T) {
+	t.Setenv("GO_TEST_VERSION", "1.17.7")
+	t.Setenv("PATH", "./testdata/gobinary/:"+os.Getenv("PATH"))
+
+	version, err := system.GoVersion()
+	testutils.RequireNoError(t, err, "expected go version to be found")
+
+	testutils.AssertEqualString(t, "1.17.7", version, "expected to find correct go version")
+}
+
+func testGoVersionFail(t *testing.T) {
+	t.Setenv("PATH", "./testdata/failing-gobinary/:"+os.Getenv("PATH"))
+
+	_, err := system.GoVersion()
+	testutils.RequireHasError(t, err, "expected to not get go version")
+
+	testutils.AssertContainsString(t, "can't get go version", err.Error(), "expected to find correct error message")
+	testutils.AssertContainsString(t, "failing go binary", err.Error(), "expected to find correct error message")
+}
